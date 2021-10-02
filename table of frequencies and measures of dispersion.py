317b49cc7852079973af8f5b727e4af92607f478
Zehcnas <izan.perezsanchez@gmail.com>
2021-10-02 16:37:29 +0200

    solve some bugs implement that work with float numbers, the fashion and the median

--- table of frequencies and measures of dispersion.py
+++ table of frequencies and measures of dispersion.py
@@ -1,8 +1,4 @@
-from typing import OrderedDict
 import numpy as np
-from numpy.lib.function_base import average
-from pandas import DataFrame
-from timeit import timeit
 from tabulate import tabulate
 
 
@@ -26,7 +22,10 @@ if typeGettingData.isnumeric():
 
         i = 0
         while i < len(Xi):
-            Xi[i] = int(Xi[i])
+            if Xi[i].__contains__('.'):
+                Xi[i] = float(Xi[i])
+            else:
+                Xi[i] = int(Xi[i])
             i += 1
 
         X = []
@@ -100,12 +99,29 @@ if typeGettingData.isnumeric():
 
         average_deviation = average_deviation / n
 
-        # variance
+        # variance fashion median
         variance = 0
+        fashion = [[0, 0]]
         for column in table:
-            variance += ((column[0] - average) ** 2 )* column[1]
+            variance += ((column[0] - average) ** 2 ) * column[1]
+
+            if column[1] == fashion[0][1]:
+                fashion.append([column[0], column[1]])
+
+            if column[1] > fashion[0][1]:
+                fashion[0][0] = column[0]
+                fashion[0][1] = column[1]
+
+
 
         variance = variance / n
+
+        median = n / 2
+        for column in table:
+            if column[2] >= median:
+                median = column[0]
+                break
+
 
 
     else:
@@ -134,10 +150,18 @@ if typeGettingData.isnumeric():
         if Xi[len(Xi) - 1] == '':
             Xi.pop()
 
+
+
         # fi
         for num in Xi:
+            numParsed = num
+            if num.__contains__('.'):
+                numParsed = float(numParsed)
+            else:
+                numParsed = int(numParsed)
+
             for category in categories:
-                if int(num) >= category[0] and int(num) < category[1]:
+                if numParsed >= category[0] and numParsed < category[1]:
                     for column in table:
                         if column[0] == category[0] and column[1] == category[1]:
                             column[2] += 1
@@ -192,19 +216,34 @@ if typeGettingData.isnumeric():
 
         average_deviation = average_deviation / n
 
-        # variance
+
+        # variance fashion median
         variance = 0
+        fashion = [[0, 0, 0]]
         for column in table:
-            variance += ((column[0] - average) ** 2 )* column[1]
+            variance += ((column[0] - average) ** 2 ) * column[2]
+
+            if column[2] == fashion[0][2]:
+                fashion.append([column[0], column[1], column[2]])
+
+            if column[1] > fashion[0][2]:
+                fashion[0][0] = column[0]
+                fashion[0][1] = column[1]
+                fashion[0][2] = column[2]
+
+
 
         variance = variance / n
 
+        median = n / 2
+        for column in table:
+            if column[3] >= median:
+                median = [column[0], column[1]]
+                break
+
     size = len(table[1])
-    print(size)
 
-    
 
-    
 
     if size == 6:
         X = []
@@ -231,9 +270,19 @@ if typeGettingData.isnumeric():
         head = ["X", "fi", 'Fi', 'hi', 'Hi', 'porcentaje']
 
         print(tabulate(info, headers = head))
+        print()
+
+        fashions = ''
+        for values in fashion:
+            fashions += f" {values[0]},"
+        fashions = fashions[:-1]
+        print(f"Moda = {fashions}")
+
+        print(f"Mediana = {median}")
 
     else:
-        X = []
+        num1 = []
+        num2 = []
         fi = []
         Fi = []
         hi = []
@@ -242,24 +291,38 @@ if typeGettingData.isnumeric():
         mark_class = []
 
         for column in table:
-            X.append(column[0])
-            fi.append(column[1])
-            Fi.append(column[2])
-            hi.append(column[3])
-            Hi.append(column[4])
-            porcentaje.append(column[5])
+            num1.append(column[0])
+            num2.append(column[1])
+            fi.append(column[2])
+            Fi.append(column[3])
+            hi.append(column[4])
+            Hi.append(column[5])
+            porcentaje.append(column[7])
             mark_class.append(column[6])
-        info = [{'X': X,
+        info = {'num1': num1,
+            'num2': num2,
             'fi': fi,
             'Fi': Fi,
             'hi': hi,
             'Hi': Hi,
             'Porcentaje': porcentaje,
-            'Marca de clase': mark_class}]
-        head = ["X", "fi", 'Fi', 'hi', 'Hi', 'porcentaje']
+            'Marca de clase': mark_class}
+        head = ["Intervalo", "Intervalo", "fi", 'Fi', 'hi', 'Hi', 'porcentaje', 'marca de clase']
         print(tabulate(info, headers = head))
+        print()
 
-    print()
+        fashions = ''
+        for values in fashion:
+            fashions += f" {values[0]} - {values[1]},"
+        fashions = fashions[:-1]
+        print(f"Moda = {fashions}")
+
+        print(f"Mediana = {median[0]} - {median[1]}")
+        print(f"Has introducido {n} numeros")
+
+
+
+
     print(f"Media = {average}")
     print(f"Desviacion mediana = {average_deviation}")
     print(f"Varianza = {variance}")
